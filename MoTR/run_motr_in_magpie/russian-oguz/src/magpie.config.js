43a21c89899432685a3f925367112c98d496354a
cuierd <cui.ding@uzh.ch>
2024-01-27 19:49:02 +0100

    set up a clean exp russian-oguz

---
+++ MoTR/run_motr_in_magpie/russian-oguz/src/magpie.config.js
@@ -0,0 +1,12 @@
+export default {
+  experimentId: '3',
+  serverUrl: 'https://cui-motr-new.herokuapp.com/',
+  socketUrl: 'wss://cui-motr-new.herokuapp.com/socket',
+  // this will be used in prolific mode
+  completionUrl: 'https://app.prolific.co/submissions/complete?cc=C1143PXS',
+  contactEmail: 'cui.ding@uzh.ch',
+  // Either 'debug', 'directLink' or 'prolific'
+  //mode: 'prolific',
+  mode: 'directLink',
+  language: 'en'
+};
