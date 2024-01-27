ab895c835fd3b98325bbd4dffd34584842a93ae1
cuierd <cui.ding@uzh.ch>
2024-01-27 23:01:26 +0100

    added prolific link

--- MoTR/run_motr_in_magpie/russian-oguz/src/magpie.config.js
+++ MoTR/run_motr_in_magpie/russian-oguz/src/magpie.config.js
@@ -3,7 +3,7 @@ export default {
   serverUrl: 'https://cui-motr-new.herokuapp.com/',
   socketUrl: 'wss://cui-motr-new.herokuapp.com/socket',
   // this will be used in prolific mode
-  completionUrl: 'https://app.prolific.co/submissions/complete?cc=C1143PXS',
+  completionUrl: 'https://app.prolific.com/submissions/complete?cc=CON33BGB',
   contactEmail: 'cui.ding@uzh.ch',
   // Either 'debug', 'directLink' or 'prolific'
   //mode: 'prolific',
